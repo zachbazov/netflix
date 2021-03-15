281ba7267ebf47811032719f6361e51b9b298a6f
Zach Bazov <zachbazov@gmail.com>
2021-03-15 05:10:29 +0200

    Initial Commit

---
+++ netflix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netflix
+//
+//  Created by Zach Bazov on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netflix/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  netflix
+//
+//  Created by Zach Bazov on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
